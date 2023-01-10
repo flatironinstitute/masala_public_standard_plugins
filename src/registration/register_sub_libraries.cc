64584410110b5f0fccbde4c21fd8c0eca8ad8459
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2023-01-10 16:45:57 -0500

    Add register_sub_libraries functions.

---
+++ src/registration/register_sub_libraries.cc
@@ -0,0 +1,40 @@
+/*
+    Standard Masala Plugins
+    Copyright (C) 2022 Vikram K. Mulligan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/// @file src/registration/register_sub_libraries.cc
+/// @brief Function to register all of the other sub-libraries with the
+/// Masala base plugin manager.
+/// @author Vikram K. Mulligan (vmulligan@flatironinstitute.org).
+
+namespace standard_masala_plugins {
+namespace registration {
+
+#include <registration/register_sub_libraries.hh>
+
+/// @brief Register sub-libraries with the Masala base plugin manager.
+void
+register_sub_libraries() {
+}
+
+/// @brief Unregister sub-libraries with the Masala base plugin manager.
+void
+unregister_sub_libraries() {
+}
+
+} // namespace registration
+} // namespace standard_masala_plugins
