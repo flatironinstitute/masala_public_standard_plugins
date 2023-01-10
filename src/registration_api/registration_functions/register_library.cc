846133eae6e6610cc55a1e756488cb0f2df66a97
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2023-01-10 16:45:20 -0500

    Move files.

--- src/registration_api/registration_functions/register_library.cc
+++ src/registration_api/registration_functions/register_library.cc
@@ -16,13 +16,15 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-/// @file src/registration_api/register_library.cc
+/// @file src/registration_api/registration_functions/register_library.cc
 /// @brief Function to register a library with the Masala base plugin manager.
 /// @author Vikram K. Mulligan (vmulligan@flatironinstitute.org).
 
 namespace standard_masala_plugins {
 namespace registration_api {
+namespace registration_functions {
 
+#include <registration_api/registration_functions/register_library.hh>
 #include <registration/register_sub_libraries.hh>
 
 /// @brief Register this library with the Masala base plugin manager.
@@ -37,5 +39,6 @@ unregister_library() {
     standard_masala_plugins::registration::unregister_sub_libraries();
 }
 
+} // namespace registration_functions
 } // namespace registration_api
 } // namespace standard_masala_plugins
