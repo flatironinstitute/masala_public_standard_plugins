6d29feda5b6c488d924e7217b2d1b6323708339f
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2023-01-24 01:15:15 -0500

    Update delete script.

--- delete_build.sh
+++ delete_build.sh
@@ -1,6 +1,7 @@
 rm -rfv bin
 rm -rfv build
 rm -rfv src/selectors_api/auto_generated_api
+rm -rfv src/optimizers_api/auto_generated_api
 rm -rfv src/registration_api/auto_generated_api
 rm -rfv headers/standard_masala_plugins
 echo "Build deleted."
