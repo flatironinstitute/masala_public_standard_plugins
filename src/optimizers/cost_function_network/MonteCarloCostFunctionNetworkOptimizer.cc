98e0c3d80cf8a807632a69b57d4114bf4cf30a05
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2023-02-02 23:12:06 -0500

    One in a million is better.

--- src/optimizers/cost_function_network/MonteCarloCostFunctionNetworkOptimizer.cc
+++ src/optimizers/cost_function_network/MonteCarloCostFunctionNetworkOptimizer.cc
@@ -458,7 +458,7 @@ MonteCarloCostFunctionNetworkOptimizer::run_mc_trajectory(
 
     // Recompute energies of all solutions to correct numerical error.
 #ifndef NDEBUG
-    solutions.recompute_all_scores( 1.0e-3 ); // As a sanity check, make sure numerical errors are small.
+    solutions.recompute_all_scores( 1.0e-6); // As a sanity check, make sure numerical errors are small.
 #else
     solutions.recompute_all_scores();
 #endif
