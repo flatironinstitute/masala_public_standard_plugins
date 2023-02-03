566bb6f92a71d0c2f1e42e885bc3a53b4b12edac
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2023-02-02 23:11:50 -0500

    Boolean option not needed.

--- src/optimizers/cost_function_network/MonteCarloCostFunctionNetworkOptimizer.cc
+++ src/optimizers/cost_function_network/MonteCarloCostFunctionNetworkOptimizer.cc
@@ -458,9 +458,9 @@ MonteCarloCostFunctionNetworkOptimizer::run_mc_trajectory(
 
     // Recompute energies of all solutions to correct numerical error.
 #ifndef NDEBUG
-    solutions.recompute_all_scores( true, 1.0e-3 ); // As a sanity check, make sure numerical errors are small.
+    solutions.recompute_all_scores( 1.0e-3 ); // As a sanity check, make sure numerical errors are small.
 #else
-    solutions.recompute_all_scores( false );
+    solutions.recompute_all_scores();
 #endif
     solutions.sort_by_score();
 
