00177c96a125ed9f1073feae9e3b9091e886c393
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2023-02-09 19:34:11 -0500

    Tweaking test formatting.

--- tests/unit/optimizers/cost_function_network/MonteCarloCostFunctionNetworkOptimizerTests.cc
+++ tests/unit/optimizers/cost_function_network/MonteCarloCostFunctionNetworkOptimizerTests.cc
@@ -178,7 +178,7 @@ TEST_CASE( "Solve a simple problem with the MonteCarloCostFunctionNetworkOptimiz
         ss << std::setw(8) << i << "\t"
             << std::setw(10) << solution->n_times_solution_was_produced() << "\t"
             << std::setw(5) << solution->solution_score() << "\t"
-            << std::setw(16) << "[" << masala::base::utility::container::container_to_string( solution->solution_at_variable_positions(), ",") << "]";
+            << " [" << masala::base::utility::container::container_to_string( solution->solution_at_variable_positions(), ",") << "]";
         tracer->write_to_tracer( "MonteCarloCostFunctionNetworkOptimizerTests", ss.str() );
     }
 
