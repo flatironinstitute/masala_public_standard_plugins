da080aef646b392dd6793cf82e2b59871dc0b290
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2023-06-24 03:25:03 -0400

    Fixing standard error.

--- src/benchmark/benchmark_apps/benchmark_monte_carlo_cfn_optimizer.cc
+++ src/benchmark/benchmark_apps/benchmark_monte_carlo_cfn_optimizer.cc
@@ -161,10 +161,11 @@ main(
                 ++counter;
             }
             avgtime /= static_cast<Real>(total_replicates);
-            //counter -= total_replicates;
+            counter -= total_replicates;
             for( Size ireplicate(0); ireplicate < total_replicates; ++ireplicate ) {
                 Real const diff(static_cast< Real >( std::get<2>(jobs[counter]) ) - avgtime);
                 std_err += diff*diff;
+                ++counter;
             }
             std_err = std::sqrt(std_err)/static_cast<Real>(total_replicates);
             if(threadcount == 1) {
