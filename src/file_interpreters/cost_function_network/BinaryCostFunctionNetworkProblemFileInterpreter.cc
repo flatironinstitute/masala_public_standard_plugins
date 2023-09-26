81ce31d1ab5b53af64512a2eb374d69df0ddc3bd
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2023-09-25 21:12:27 -0400

    Initial commit of BinaryCostFunctionNetworkProblemFileInterpreter.

---
+++ src/file_interpreters/cost_function_network/BinaryCostFunctionNetworkProblemFileInterpreter.cc
@@ -0,0 +1,222 @@
+/*
+    Masala
+    Copyright (C) 2022 Vikram K. Mulligan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/// @file src/file_interpreters/cost_function_network/BinaryCostFunctionNetworkProblemFileInterpreter.cc
+/// @brief Implementation for a CostFunctionNetworkOptimizer that solves a cost function network problem using Monte
+/// Carlo methods.
+/// @details This performs a Metropolis-Hastings Monte Carlo search of node setting space, where each move is to
+/// pick a node at random and change its setting at random, compute the change in overall energy or score, and
+/// accept or reject the move based on the difference in energy and the Metropolis criterion.
+/// @note If the annealing schedule used ramps temperature, this does simulated annealing.
+/// @author Vikram K. Mulligan (vmulligan@flatironinstitute.org).
+
+// Unit header:
+#include <file_interpreters/cost_function_network/BinaryCostFunctionNetworkProblemFileInterpreter.hh>
+
+// Numeric API headers:
+#include <numeric_api/auto_generated_api/optimization/cost_function_network/CostFunctionNetworkOptimizationProblem_API.hh>
+#include <numeric_api/auto_generated_api/optimization/cost_function_network/CostFunctionNetworkOptimizationSolution_API.hh>
+#include <numeric_api/auto_generated_api/optimization/cost_function_network/CostFunctionNetworkOptimizationProblems_API.hh>
+#include <numeric_api/auto_generated_api/optimization/cost_function_network/CostFunctionNetworkOptimizationSolutions_API.hh>
+#include <numeric_api/auto_generated_api/optimization/annealing/AnnealingScheduleBase_API.hh>
+
+// Base headers:
+#include <base/error/ErrorHandling.hh>
+#include <base/api/MasalaObjectAPIDefinition.hh>
+#include <base/api/constructor/MasalaObjectAPIConstructorMacros.hh>
+#include <base/api/setter/MasalaObjectAPISetterDefinition_OneInput.tmpl.hh>
+#include <base/api/getter/MasalaObjectAPIGetterDefinition_ZeroInput.tmpl.hh>
+#include <base/api/work_function/MasalaObjectAPIWorkFunctionDefinition_ZeroInput.tmpl.hh>
+#include <base/api/work_function/MasalaObjectAPIWorkFunctionDefinition_OneInput.tmpl.hh>
+#include <base/managers/threads/MasalaThreadManager.hh>
+#include <base/managers/threads/MasalaThreadedWorkRequest.hh>
+#include <base/managers/threads/MasalaThreadedWorkExecutionSummary.hh>
+#include <base/managers/random/MasalaRandomNumberGenerator.hh>
+#include <base/managers/plugin_module/MasalaPluginModuleManager.hh>
+#include <base/utility/container/container_util.tmpl.hh>
+
+// STL headers:
+#include <vector>
+#include <string>
+#include <utility>
+#include <sstream>
+#include <iostream>
+
+namespace standard_masala_plugins {
+namespace file_interpreters {
+namespace cost_function_network {
+
+////////////////////////////////////////////////////////////////////////////////
+// CONSTRUCTION AND DESTRUCTION
+////////////////////////////////////////////////////////////////////////////////
+
+/// @brief Copy constructor.
+/// @details Needed since we define a mutex.
+BinaryCostFunctionNetworkProblemFileInterpreter::BinaryCostFunctionNetworkProblemFileInterpreter(
+    BinaryCostFunctionNetworkProblemFileInterpreter const & src
+) :
+    masala::numeric_api::base_classes::optimization::cost_function_network::CostFunctionNetworkOptimizer( src )
+{
+    std::lock_guard< std::mutex > lock( src.file_interpreter_mutex_ );
+    //TODO TODO TODO
+}
+
+/// @brief Assignment operator.
+/// @details Needed since we define a mutex.
+BinaryCostFunctionNetworkProblemFileInterpreter &
+BinaryCostFunctionNetworkProblemFileInterpreter::operator=( BinaryCostFunctionNetworkProblemFileInterpreter const & src ) {
+    std::lock( file_interpreter_mutex_, src.file_interpreter_mutex_ );
+    std::lock_guard< std::mutex > lock1( file_interpreter_mutex_, std::adopt_lock );
+    std::lock_guard< std::mutex > lock2( src.file_interpreter_mutex_, std::adopt_lock );
+    // TODO TODO TODO
+    return *this;
+}
+
+/// @brief Make a copy of this object that's wholly independent.
+BinaryCostFunctionNetworkProblemFileInterpreterSP
+BinaryCostFunctionNetworkProblemFileInterpreter::deep_clone() const {
+    BinaryCostFunctionNetworkProblemFileInterpreterSP new_optimizer( masala::make_shared< BinaryCostFunctionNetworkProblemFileInterpreter >(*this) );
+    new_optimizer->make_independent();
+    return new_optimizer;
+}
+
+/// @brief Make this object independent of any of its copies (i.e. deep-clone all of its internal data).
+void
+BinaryCostFunctionNetworkProblemFileInterpreter::make_independent() {
+    if( annealing_schedule_ != nullptr ) {
+        annealing_schedule_ = annealing_schedule_->deep_clone();
+    }
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// PUBLIC MEMBER FUNCTIONS
+////////////////////////////////////////////////////////////////////////////////
+
+/// @brief Get the category or categories for this plugin class.
+/// @returns { { "Optimizer", "CostFunctionNetworkOptimizer" } }
+/// @note Categories are hierarchical (e.g. Selector->AtomSelector->AnnotatedRegionSelector,
+/// stored as { {"Selector", "AtomSelector", "AnnotatedRegionSelector"} }). A plugin can be
+/// in more than one hierarchical category (in which case there would be more than one
+/// entry in the outher vector), but must be in at least one.  The first one is used as
+/// the primary key.
+std::vector< std::vector< std::string > >
+BinaryCostFunctionNetworkProblemFileInterpreter::get_categories() const {
+    using namespace masala::numeric_api::base_classes::optimization::cost_function_network;
+	return { { "FileInterpreter", "CostFunctionNetworkProblemFileInterpreter", "BinaryCostFunctionNetworkProblemFileInterpreter" } }
+}
+
+/// @brief Get the keywords for this plugin class.
+/// @returns { "file_interpreter", "cost_function_network", "problem", "binary" }
+std::vector< std::string >
+BinaryCostFunctionNetworkProblemFileInterpreter::get_keywords() const {
+    return { "file_interpreter", "cost_function_network", "problem", "binary" };
+}
+
+/// @brief Get the class name.
+/// @returns "BinaryCostFunctionNetworkProblemFileInterpreter".
+std::string
+BinaryCostFunctionNetworkProblemFileInterpreter::class_name() const {
+    return class_name_static();
+}
+
+/// @brief Get the class namespace.
+/// @returns "standard_masala_plugins::file_interpreters::cost_function_network".
+std::string
+BinaryCostFunctionNetworkProblemFileInterpreter::class_namespace() const {
+    return class_namespace_static();
+}
+
+/// @brief Get the class name (static version).
+/// @returns "BinaryCostFunctionNetworkProblemFileInterpreter".
+//static
+std::string
+BinaryCostFunctionNetworkProblemFileInterpreter::class_name_static() {
+    return "BinaryCostFunctionNetworkProblemFileInterpreter";
+}
+
+/// @brief Get the class namespace (static version).
+/// @returns "standard_masala_plugins::file_interpreters::cost_function_network".
+//static
+std::string
+BinaryCostFunctionNetworkProblemFileInterpreter::class_namespace_static() {
+    return "standard_masala_plugins::file_interpreters::cost_function_network";
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// PUBLIC API FUNCTION
+////////////////////////////////////////////////////////////////////////////////
+
+/// @brief Get the API definition for this object.
+masala::base::api::MasalaObjectAPIDefinitionCWP
+BinaryCostFunctionNetworkProblemFileInterpreter::get_api_definition() {
+    using namespace masala::base::api;
+    using namespace masala::base::api::constructor;
+    using namespace masala::base::api::setter;
+    using namespace masala::base::api::getter;
+    using namespace masala::base::api::work_function;
+    using namespace masala::numeric_api::auto_generated_api::optimization::annealing;
+    using masala::base::Size;
+
+    std::lock_guard< std::mutex > lock( file_interpreter_mutex_ );
+    if( api_description_ == nullptr ) {
+        MasalaObjectAPIDefinitionSP api_description(
+            masala::make_shared< MasalaObjectAPIDefinition >(
+                *this,
+                "TODO TODO TODO",
+                false, false
+            )
+        );
+
+        // Constructors:
+        ADD_PUBLIC_CONSTRUCTOR_DEFINITIONS( BinaryCostFunctionNetworkProblemFileInterpreter, api_description );
+
+        // Setters:
+
+
+		// Getters:
+
+
+        // Work functions:
+
+        // Convert nonconst to const:
+        api_description_ = api_description;
+    }
+    return api_description_;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// PUBLIC SETTERS
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+// PUBLIC GETTERS
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+// PUBLIC WORK FUNCTIONS
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+// PRIVATE FUNCTIONS
+////////////////////////////////////////////////////////////////////////////////
+
+
+} // namespace cost_function_network
+} // namespace file_interpreters
+} // namesapce standard_masala_plugins
---
+++ src/file_interpreters/cost_function_network/BinaryCostFunctionNetworkProblemFileInterpreter.hh
@@ -0,0 +1,165 @@
+/*
+    Masala
+    Copyright (C) 2022 Vikram K. Mulligan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/// @file src/file_interpreters/cost_function_network/BinaryCostFunctionNetworkProblemFileInterpreter.hh
+/// @brief Headers for a file interpreter for reading and writing binary cost function network
+/// optimization problems.
+/// @author Vikram K. Mulligan (vmulligan@flatironinstitute.org).
+
+#ifndef StandardMaslaPlugins_src_file_interpreters_cost_function_network_BinaryCostFunctionNetworkProblemFileInterpreter_hh
+#define StandardMaslaPlugins_src_file_interpreters_cost_function_network_BinaryCostFunctionNetworkProblemFileInterpreter_hh
+
+// Forward declarations:
+#include <file_interpreters/cost_function_network/BinaryCostFunctionNetworkProblemFileInterpreter.fwd.hh>
+
+// Parent header:
+#include <numeric_api/base_classes/optimization/cost_function_network/CostFunctionNetworkOptimizer.hh>
+
+// Base headers:
+#include <base/types.hh>
+#include <base/managers/file_interpreter/MasalaFileInterpreter.hh>
+
+// STL headers:
+#include <mutex>
+
+namespace standard_masala_plugins {
+namespace file_interpreters {
+namespace cost_function_network {
+
+/// @brief A file interpreter for reading and writing binary cost function network
+/// optimization problems.
+/// @author Vikram K. Mulligan (vmulligan@flatironinstitute.org).
+class BinaryCostFunctionNetworkProblemFileInterpreter : public masala::base::managers::file_interpreter::MasalaFileInterpreter {
+
+public:
+
+////////////////////////////////////////////////////////////////////////////////
+// CONSTRUCTION AND DESTRUCTION
+////////////////////////////////////////////////////////////////////////////////
+
+	/// @brief Default constructor.
+	BinaryCostFunctionNetworkProblemFileInterpreter() = default;
+
+	/// @brief Copy constructor.
+	/// @details Needed since we define a mutex.
+	BinaryCostFunctionNetworkProblemFileInterpreter( BinaryCostFunctionNetworkProblemFileInterpreter const & );
+
+	/// @brief Assignment operator.
+	/// @details Needed since we define a mutex.
+	BinaryCostFunctionNetworkProblemFileInterpreter &
+	operator=( BinaryCostFunctionNetworkProblemFileInterpreter const & );
+
+	/// @brief Make a copy of this object that's wholly independent.
+	BinaryCostFunctionNetworkProblemFileInterpreterSP
+	deep_clone() const;
+
+	/// @brief Make this object independent of any of its copies (i.e. deep-clone all of its internal data).
+	void
+	make_independent();
+
+	/// @brief Destructor.
+	~BinaryCostFunctionNetworkProblemFileInterpreter() override = default;
+
+public:
+
+////////////////////////////////////////////////////////////////////////////////
+// PUBLIC MEMBER FUNCTIONS
+////////////////////////////////////////////////////////////////////////////////
+
+	/// @brief Get the category or categories for this plugin class.
+	/// @returns { { "FileInterpreter", "CostFunctionNetworkProblemFileInterpreter", "BinaryCostFunctionNetworkProblemFileInterpreter" } }
+	/// @note Categories are hierarchical (e.g. Selector->AtomSelector->AnnotatedRegionSelector,
+	/// stored as { {"Selector", "AtomSelector", "AnnotatedRegionSelector"} }). A plugin can be
+	/// in more than one hierarchical category (in which case there would be more than one
+	/// entry in the outher vector), but must be in at least one.  The first one is used as
+	/// the primary key.
+	std::vector< std::vector< std::string > >
+	get_categories() const override;
+
+	/// @brief Get the keywords for this plugin class.
+	/// @returns { "file_interpreter", "cost_function_network", "problem", "binary" }
+	std::vector< std::string >
+	get_keywords() const override;
+
+	/// @brief Get the class name.
+	/// @returns "BinaryCostFunctionNetworkProblemFileInterpreter".
+	std::string class_name() const override;
+
+	/// @brief Get the class namespace.
+	/// @returns "standard_masala_plugins::file_interpreters::cost_function_network".
+	std::string class_namespace() const override;
+
+	/// @brief Get the class name (static version).
+	/// @returns "BinaryCostFunctionNetworkProblemFileInterpreter".
+	static std::string class_name_static();
+
+	/// @brief Get the class namespace (static version).
+	/// @returns "standard_masala_plugins::file_interpreters::cost_function_network".
+	static std::string class_namespace_static();
+
+public:
+
+////////////////////////////////////////////////////////////////////////////////
+// PUBLIC API FUNCTION
+////////////////////////////////////////////////////////////////////////////////
+
+	/// @brief Get the API definition for this object.
+	masala::base::api::MasalaObjectAPIDefinitionCWP
+	get_api_definition();
+
+public:
+
+////////////////////////////////////////////////////////////////////////////////
+// PUBLIC SETTERS
+////////////////////////////////////////////////////////////////////////////////
+
+
+public:
+
+////////////////////////////////////////////////////////////////////////////////
+// PUBLIC GETTERS
+////////////////////////////////////////////////////////////////////////////////
+
+public:
+
+////////////////////////////////////////////////////////////////////////////////
+// PUBLIC WORK FUNCTIONS
+////////////////////////////////////////////////////////////////////////////////
+
+private:
+
+////////////////////////////////////////////////////////////////////////////////
+// PRIVATE FUNCTIONS
+////////////////////////////////////////////////////////////////////////////////
+
+private:
+
+////////////////////////////////////////////////////////////////////////////////
+// PRIVATE DATA
+////////////////////////////////////////////////////////////////////////////////
+
+	/// @brief A mutex for threadsafe operation.
+	mutable std::mutex file_interpreter_mutex_;
+
+}; // class BinaryCostFunctionNetworkProblemFileInterpreter
+
+} // namespace cost_function_network
+} // namespace file_interpreters
+} // namesapce standard_masala_plugins
+
+#endif // StandardMaslaPlugins_src_file_interpreters_cost_function_network_BinaryCostFunctionNetworkProblemFileInterpreter_hh
