14a8ad9ff2b40f30ab6e636f37726d8e40c19640
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2024-04-22 14:46:06 -0400

    Adding test of loading const scoring term from plugin manager.

--- tests/unit/scoring/scoring_terms/molecular_system/ConstantScoringTermTests.cc
+++ tests/unit/scoring/scoring_terms/molecular_system/ConstantScoringTermTests.cc
@@ -33,6 +33,10 @@
 
 // Base headers:
 #include <base/types.hh>
+#include <base/managers/plugin_module/MasalaPluginModuleManager.hh>
+#include <base/api/MasalaObjectAPIDefinition.hh>
+#include <base/api/work_function/MasalaObjectAPIWorkFunctionDefinition_FourInput.tmpl.hh>
+#include <base/api/setter/MasalaObjectAPISetterDefinition_OneInput.tmpl.hh>
 
 namespace standard_masala_plugins {
 namespace tests {
@@ -71,6 +75,58 @@ TEST_CASE( "Score two empty molecular systems", "[standard_masala_plugins::auto_
     CHECK( scores[1] == 0.25 );
     const_scoreterm->write_to_tracer( "Score1: " + std::to_string(scores[0]) );
     const_scoreterm->write_to_tracer( "Score2: " + std::to_string(scores[1]) );
+}
+
+TEST_CASE( "Score two empty molecular systems, loading the score term from the plugin system", "[standard_masala_plugins::auto_generated_api::scoring_terms::molecular_system::ConstantScoringTerm_API][scoring][score]" ) {
+    using namespace scoring_api::auto_generated_api::scoring_terms::molecular_system;
+    using namespace masala::core_api::auto_generated_api::molecular_system;
+	using namespace masala::base::api;
+	using namespace masala::base::managers::plugin_module;
+
+	standard_masala_plugins::registration_api::register_library();
+
+	MasalaPluginAPISP scoreterm;
+    std::vector< masala::base::Real > scores;
+    REQUIRE_NOTHROW([&](){
+		scoreterm = MasalaPluginModuleManager::get_instance()->create_plugin_object_instance_by_short_name( { "ScoringTerm" }, "ConstantScoreTerm", true );
+		MasalaObjectAPIDefinitionCSP api_def( scoreterm->get_api_definition_for_inner_class() );
+		REQUIRE( api_def != nullptr );
+		
+		setter::MasalaObjectAPISetterDefinition_OneInputCSP< masala::base::Real > setterfxn(
+			api_def->get_oneinput_setter_function< masala::base::Real >( "set_constant_value ")
+		);
+		setterfxn->function( 0.25 );
+
+		work_function::MasalaObjectAPIWorkFunctionDefinition_FourInputCSP<
+			std::vector< masala::base::Real >,
+			std::vector< MolecularSystem_APICSP > const &,
+			std::vector< masala::core_api::auto_generated_api::scoring::ScoringTermAdditionalInput_APICSP > const * const,
+			std::vector< masala::core_api::auto_generated_api::scoring::ScoringTermCache_APISP > const * const,
+			std::vector< masala::core_api::auto_generated_api::scoring::ScoringTermAdditionalOutput_APICSP > * const
+		> score_workfxn(
+			api_def->get_fourinput_work_function<
+				std::vector< masala::base::Real >,
+				std::vector< MolecularSystem_APICSP > const &,
+				std::vector< masala::core_api::auto_generated_api::scoring::ScoringTermAdditionalInput_APICSP > const * const,
+				std::vector< masala::core_api::auto_generated_api::scoring::ScoringTermCache_APISP > const * const,
+				std::vector< masala::core_api::auto_generated_api::scoring::ScoringTermAdditionalOutput_APICSP > * const
+			>( std::string( "score" ) )
+		);
+
+        MolecularSystem_APISP molecular_system1( masala::make_shared< MolecularSystem_API >() );
+        MolecularSystem_APISP molecular_system2( masala::make_shared< MolecularSystem_API >() );
+        std::vector< MolecularSystem_APICSP > molsys_vec{ molecular_system1, molecular_system2 };
+
+        scoreterm->write_to_tracer( "Preparing to score two empty molecular systems." );
+        scores = score_workfxn->function( molsys_vec, nullptr, nullptr, nullptr );
+    }() );
+    REQUIRE( scores.size() == 2 );
+    CHECK( scores[0] == 0.25 );
+    CHECK( scores[1] == 0.25 );
+    scoreterm->write_to_tracer( "Score1: " + std::to_string(scores[0]) );
+    scoreterm->write_to_tracer( "Score2: " + std::to_string(scores[1]) );
+
+	standard_masala_plugins::registration_api::unregister_library();
 }
 
 } // namespace molecular_system
