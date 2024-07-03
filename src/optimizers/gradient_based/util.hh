fb0696f863580e57bdaa1ae7d077427706b25753
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2024-07-03 01:31:37 -0400

    Initial commit of util.hh.

---
+++ src/optimizers/gradient_based/util.hh
@@ -0,0 +1,42 @@
+/*
+    Masala
+    Copyright (C) 2024 Vikram K. Mulligan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/// @file src/numeric_api/base_classes/optimization/gradient_based/util.hh
+/// @brief Inlined utility functions for gradient-descent optimizers.
+/// @details This is a header-only set of mathematical utility functions.  Since these are entirely
+/// defined in the header and inlined, inclusion of this file results in efficient machine code,
+/// at the cost of slightly greater compilation time. 
+/// @author Vikram K. Mulligan (vmulligan@flatironinstitute.org).
+
+#ifndef Standard_Masala_Plugins_src_optimizers_gradient_based_util_hh
+#define Standard_Masala_Plugins_src_optimizers_gradient_based_util_hh
+
+// Base headers:
+#include <base/types.hh>
+
+namespace standard_masala_plugins {
+namespace optimizers {
+namespace gradient_based {
+
+
+
+} // namespace gradient_based
+} // namespace optimizers
+} // namespace standard_masala_plugins
+
+#endif // Standard_Masala_Plugins_src_optimizers_gradient_based_util_hh
