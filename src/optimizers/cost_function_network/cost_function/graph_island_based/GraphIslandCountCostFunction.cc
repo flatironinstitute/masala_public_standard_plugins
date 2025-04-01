69359d7ac3984120266d382010a01286f305b864
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2025-04-01 12:40:39 -0400

    One mistake found.

--- src/optimizers/cost_function_network/cost_function/graph_island_based/GraphIslandCountCostFunction.cc
+++ src/optimizers/cost_function_network/cost_function/graph_island_based/GraphIslandCountCostFunction.cc
@@ -634,7 +634,7 @@ GraphIslandCountCostFunction::do_add(
 	}
 	DEBUG_MODE_CHECK_OR_THROW_FOR_CLASS( found1 == found2, "do_add", "Program error: asymmetric edge found." );
 #endif
-	if( found1 ) {
+	if( !found1 ) {
 		edges_for_node_in_connectivity_graph[pair_to_add.first][nedges_for_node_in_connectivity_graph[pair_to_add.first]] = pair_to_add.second;
 		edges_for_node_in_connectivity_graph[pair_to_add.second][nedges_for_node_in_connectivity_graph[pair_to_add.second]] = pair_to_add.first;
 		++(nedges_for_node_in_connectivity_graph[pair_to_add.first]);
