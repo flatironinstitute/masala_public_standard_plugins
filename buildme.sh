6cd4324c3b641d56434fcb5a6b0c63e770b754ed
Vikram K. Mulligan <vmulligan@flatironinstitute.org>
2025-05-23 17:50:44 -0400

    Updating buildme.

--- buildme.sh
+++ buildme.sh
@@ -1,4 +1,6 @@
 mkdir bin && echo "Created bin/ directory." || echo "Directory bin/ already exists."
+mkdir bin/tests && echo "Created bin/tests directory." || echo "Directory bin/tests already exists."
+mkdir bin/api && echo "Created bin/api directory." || echo "Directory bin/api already exists."
 mkdir build && echo "Created build/ directory." || echo "Directory build/ already exists."
 cd build
 mkdir cmake_generated && echo "Created build/cmake_generated/ sub-directory." || echo "Sub-directory build/cmake_generated/ already exists."
@@ -6,8 +8,14 @@ cmake ../cmake/
 make -j `nproc`
 
 test "${PIPESTATUS[0]}" != '0' && { echo "Build errors!  Check output log for details."; exit 1; } || { echo "Build completed."; }
-
 cd ..
+
+cd bin/tests
+ln -s ../../build/*_tests ./
+cd ../api
+ln -s ../../build/generate_*_api ./
+cd ../..
+
 mkdir headers/standard_masala_plugins && echo "Created headers/standard_masala_plugins/ directory." || echo "Directory headers/standard_masala_plugins/ already exists."
 rm -r headers/standard_masala_plugins/*
 python3 ./code_templates/copy_headers.py standard_masala_plugins selectors_api src headers/standard_masala_plugins
